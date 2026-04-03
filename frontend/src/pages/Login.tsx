ed43f88c493e437731d843a835ca93b301fcdd3b
Kalyango Timothy <timothykalyango@gmail.com>
2026-04-03 18:39:18 +0300

    Auto-commit: 2026-04-03 18:39:18

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,7 +13,6 @@ import { Input } from "@/components/ui/Input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/Button";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Separator } from "@/components/ui/separator";
 import {
   Eye,
   EyeOff,
