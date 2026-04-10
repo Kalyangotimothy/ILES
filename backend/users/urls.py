89f9b700788652c0d4ed8a0708f683cbf4af97f0
Kalyango Timothy <timothykalyango@gmail.com>
2026-04-10 23:21:16 +0300

    Auto-commit: 2026-04-10 23:21:16

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,16 +1,7 @@
 from django.urls import path
-from .views import MeView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from .views import MeView, UsersListView
 
 urlpatterns = [
     path('me/', MeView.as_view(), name='user-me'),
+    path('', UsersListView.as_view(), name='users-list'),
 ]
-class MeView(APIView):
-    permission_classes = [IsAuthenticated] # Ensures only logged-in users enter
-
-    def get(self, request):
-        serializer = UserSerializer(request.user)
-        return Response(serializer.data)
-
