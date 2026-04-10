b46eec34e8940148b6294eab7735bdb39d11528f
hector904 <hecta.madete@gmail.com>
2026-04-10 15:23:06 +0300

    Fix comment syntax in LogbookConfig

--- backend/logbook/apps.py
+++ backend/logbook/apps.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 class LogbookConfig(AppConfig):
     name = 'logbook'
-    // This makes 'Logbook' appear as 'Logbook Management' in the Admin UI
+    # This makes 'Logbook' appear as 'Logbook Management' in the Admin UI
     verbose_name = _('Logbook Management')
     default_auto_field = 'django.db.models.BigAutoField'
