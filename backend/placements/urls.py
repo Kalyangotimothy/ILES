13fd1d66fb9039c22a7de33657c164a41f9098eb
Kalyango Timothy <timothykalyango@gmail.com>
2026-04-16 22:22:47 +0300

    Auto-commit: 2026-04-16 22:22:46

--- backend/placements/urls.py
+++ backend/placements/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PlacementViewSet
+from .views import PlacementViewSet, PlacementDocumentViewSet
 
 router = DefaultRouter()
 router.register('', PlacementViewSet, basename='placement')
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Document endpoints
+    path('documents/', PlacementDocumentViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    }), name='placement-documents-list'),
+    path('documents/<int:pk>/', PlacementDocumentViewSet.as_view({
+        'get': 'retrieve',
+        'delete': 'destroy'
+    }), name='placement-documents-detail'),
 ]
